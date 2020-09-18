883626ca56bd8973f5d707fba2e3a0bf99702c82
Gabriel Ventrice <gvcapanema@gmail.com>
2020-09-18 16:57:25 -0300

    remove static token

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,7 +15,7 @@ api.interceptors.request.use(config => {
 
   if (token) {
     Object.assign(config.headers, {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VySWQiOiI1ZDMwZDdjNDFjOWQ0NDAwMDA4Zjk0N2QiLCJpYXQiOjE1OTg5Nzg5NDZ9.tQ5pluUrLaSkaT-l9NCCKAyiOWyBajdzAJKDzcP6F8Y`
+      Authorization: `Bearer ${token}`
     })
   }
 
